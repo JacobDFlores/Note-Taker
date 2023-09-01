bbcb2dd2f80f34457152b8af3d37d874372b3b23
JacobDFlores <jacobfdaniel@gmail.com>
2023-08-31 20:00:36 -0500

    finished backend

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const notesRouter = require('./notes');
 
-// Import our modular routers for /tips and /feedback
+// Import our modular routers for /notes
 
 const app = express();
 
+app.use('/notes', notesRouter);
 
 module.exports = app;
